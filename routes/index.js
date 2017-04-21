0593aecf9f679961d1dfd5e38b310676f847545a
SeniYuting <2580238659@qq.com>
2017-04-21 19:43:34 +0800

    reformat

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Editor Demo' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Editor Demo'});
 });
 
-router.get('/medium_editor', function(req, res, next) {
-    res.render('index_medium_editor', { title: 'Medium Editor Demo' });
+router.get('/medium_editor', function (req, res) {
+    res.render('index_medium_editor', {title: 'Medium Editor Demo'});
 });
 
-router.get('/draft', function(req, res, next) {
-    res.render('index_draft', { title: 'Draft Demo' });
+router.get('/draft', function (req, res) {
+    res.render('index_draft', {title: 'Draft Demo'});
 });
 
 module.exports = router;
