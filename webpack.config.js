45b91de8ce6b4fe830a51c0b42d99c62853c9b27
Victor <1341634255@qq.com>
2017-04-22 02:38:57 +0800

    update .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Victor_Zhou on 2017-4-22.
+ */
+module.exports = {
+    entry: [
+        './public/javascripts/Draft/index/entry.jsx'
+    ],
+    output: {
+        path: __dirname + '/public/javascripts/Draft',
+        publicPath: "/public/javascripts/Draft",
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+};
