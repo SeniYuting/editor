56f1913891094db704d8780867309a6dca89857f
Victor <1341634255@qq.com>
2017-04-21 13:31:33 +0800

    medium editor & draft

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Editor-Demo' });
 });
 
+router.get('/medium_editor', function(req, res, next) {
+
+    res.redirect('index_medium_editor');
+});
+
 module.exports = router;
